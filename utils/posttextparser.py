8b94b647adb08eb5b8d6a757ce075747d6441c7e
MahiRavanKRG <adi.chat16.codes@gmail.com>
2023-06-17 18:07:27 +0530

    My first contribution
    please see this as a first try.

--- utils/posttextparser.py
+++ utils/posttextparser.py
@@ -11,6 +11,16 @@ from utils.voice import sanitize_text
 
 # working good
 def posttextparser(obj, *, tried: bool = False) -> List[str]:
+    """
+    The function takes in a string object, replaces newline characters with spaces, and attempts to load
+    a spacy model for English language processing. If the model cannot be loaded, it will attempt to
+    download it and try again. If it still cannot be loaded, it will print an error message and raise an
+    exception. The function returns a list of strings.
+    @param obj - The input text that needs to be parsed and processed.
+    @param {bool} [tried=False] - A boolean parameter that indicates whether the function has already
+    attempted to load the spacy model and failed. It is set to False by default.
+    @returns a list of strings.
+    """
     text: str = re.sub("\n", " ", obj)
     try:
         nlp = spacy.load("en_core_web_sm")
